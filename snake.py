682d29cda36dad5e6dae3ec8e85a3abf21def845
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-10-10 19:11:06 +0100

    add snake on screen

--- snake.py
+++ snake.py
@@ -2,13 +2,28 @@
 import pygame
 from pygame.locals import *
 
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
 pygame.init()
-screen = pygame.display.set_mode((600,600))
+screen = pygame.display.set_mode((600, 600))
+
+snake_skin = pygame.Surface((10, 10))
+snake_skin.fill((255, 255, 255))
+
+snake = [(200, 200), (210, 200), (220, 200)]
+my_directio = LEFT
 
 while True:
 
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      pygame.quit()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
 
-  pygame.display.update()
+    screen.fill((0, 0, 0))
+    for pos in snake:
+        screen.blit(snake_skin, pos)
+
+    pygame.display.update()
