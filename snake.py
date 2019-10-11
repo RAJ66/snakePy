3f5714f1b6130abbd280aa0095438bfa5e98827a
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-10-11 11:23:26 +0100

    add apple on screen

--- snake.py
+++ snake.py
@@ -1,5 +1,4 @@
-
-import pygame
+import pygame, random
 from pygame.locals import *
 
 UP = 0
@@ -13,6 +12,10 @@ screen = pygame.display.set_mode((600, 600))
 snake_skin = pygame.Surface((10, 10))
 snake_skin.fill((255, 255, 255))
 
+apple = pygame.Surface((10,10))
+apple.fill((255,0,0))
+apple_pos = (random.randint(0,590),random.randint(0,590))
+
 snake = [(200, 200), (210, 200), (220, 200)]
 my_directio = LEFT
 
@@ -23,6 +26,8 @@ while True:
             pygame.quit()
 
     screen.fill((0, 0, 0))
+
+    screen.blit(apple,apple_pos)
     for pos in snake:
         screen.blit(snake_skin, pos)
 
