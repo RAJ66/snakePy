d4c2f277c209f1fc95695a2f46c6bc09a55fce3c
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-10-11 11:40:31 +0100

    move snake

--- snake.py
+++ snake.py
@@ -18,12 +18,34 @@ apple_pos = (random.randint(0,590),random.randint(0,590))
 
 snake = [(200, 200), (210, 200), (220, 200)]
 my_directio = LEFT
+clock = pygame.time.Clock()
 
 while True:
-
+    clock.tick(20)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                my_directio =UP
+            if event.key == K_DOWN:
+                my_directio =DOWN
+            if event.key == K_LEFT:
+                my_directio =LEFT
+            if event.key == K_RIGHT:
+             my_directio= RIGHT
+
+    if my_directio == UP:
+        snake[0] = (snake[0][0],snake[0][1]-10)
+    if my_directio == DOWN:
+        snake[0] = (snake[0][0],snake[0][1]+10)
+    if my_directio == RIGHT:
+        snake[0] = (snake[0][0]+10,snake[0][1])
+    if my_directio == LEFT:
+        snake[0] = (snake[0][0]-10,snake[0][1])
+
+    for i in range(len(snake)-1,0,-1):
+        snake[i] = (snake[i-1][0],snake[i-1][1])
 
     screen.fill((0, 0, 0))
 
