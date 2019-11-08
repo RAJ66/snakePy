5c58b12c84c214822d1b666345ba598be4880743
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-11-08 19:03:41 +0000

    add grid

--- snake.py
+++ snake.py
@@ -1,14 +1,18 @@
-
-import pygame, random
+# https://github.com/filhoweuler/Pygame-Snake/blob/master/Solu%C3%A7%C3%A3o%20-%20Snake.pdf
+import pygame
+import random
 from pygame.locals import *
 
-def on_grid_rando():
-    x=random.randint(0,590)
-    y=random.randint(0,590)
-    return(x//10*10,y//10*10)
 
-def collision(c1,c2):
-    return (c1[0] ==c2[0])and(c1[1] ==c2[1])
+def on_grid_rando():
+    x = random.randint(0, 590)
+    y = random.randint(0, 590)
+    return(x//10*10, y//10*10)
+
+
+def collision(c1, c2):
+    return (c1[0] == c2[0])and(c1[1] == c2[1])
+
 
 UP = 0
 RIGHT = 1
@@ -21,8 +25,8 @@ screen = pygame.display.set_mode((600, 600))
 snake_skin = pygame.Surface((10, 10))
 snake_skin.fill((255, 255, 255))
 
-apple = pygame.Surface((10,10))
-apple.fill((255,0,0))
+apple = pygame.Surface((10, 10))
+apple.fill((255, 0, 0))
 apple_pos = (on_grid_rando())
 
 snake = [(200, 200), (210, 200), (220, 200)]
@@ -36,45 +40,48 @@ while True:
             pygame.quit()
         if event.type == KEYDOWN:
             if event.key == K_UP:
-                my_directio =UP
+                my_directio = UP
             if event.key == K_DOWN:
-                my_directio =DOWN
+                my_directio = DOWN
             if event.key == K_LEFT:
-                my_directio =LEFT
+                my_directio = LEFT
             if event.key == K_RIGHT:
-             my_directio= RIGHT
+                my_directio = RIGHT
 
-    if   collision(snake[0],apple_pos):
-        apple_pos  = on_grid_rando()
-        snake.append((0,0))  
+    if collision(snake[0], apple_pos):
+        apple_pos = on_grid_rando()
+        snake.append((0, 0))
 
-
-    if snake[0][0] ==600 or snake[0][1] ==600 or snake[0][0] <0 or snake[0][1] <0:
+    if snake[0][0] == 600 or snake[0][1] == 600 or snake[0][0] < 0 or snake[0][1] < 0:
         pygame.quit()
-        exit() 
+        exit()
 
-    for i in range(1,len(snake)-1):
-        if snake[0][0] ==snake[i][0] and snake[0][1] ==snake[i][1]:
+    for i in range(1, len(snake)-1):
+        if snake[0][0] == snake[i][0] and snake[0][1] == snake[i][1]:
             pygame.quit()
             exit()
 
-    for i in range(len(snake)-1,0,-1):
-        snake[i] = (snake[i-1][0],snake[i-1][1])    
+    for i in range(len(snake)-1, 0, -1):
+        snake[i] = (snake[i-1][0], snake[i-1][1])
 
     if my_directio == UP:
-        snake[0] = (snake[0][0],snake[0][1]-10)
+        snake[0] = (snake[0][0], snake[0][1]-10)
     if my_directio == DOWN:
-        snake[0] = (snake[0][0],snake[0][1]+10)
+        snake[0] = (snake[0][0], snake[0][1]+10)
     if my_directio == RIGHT:
-        snake[0] = (snake[0][0]+10,snake[0][1])
+        snake[0] = (snake[0][0]+10, snake[0][1])
     if my_directio == LEFT:
-        snake[0] = (snake[0][0]-10,snake[0][1])
-
-    
+        snake[0] = (snake[0][0]-10, snake[0][1])
 
     screen.fill((0, 0, 0))
 
-    screen.blit(apple,apple_pos)
+    screen.blit(apple, apple_pos)
+
+    for x in range(0, 600, 10):  # Draw vertical lines
+        pygame.draw.line(screen, (40, 40, 40), (x, 0), (x, 600))
+    for y in range(0, 600, 10):  # Draw vertical lines
+        pygame.draw.line(screen, (40, 40, 40), (0, y), (600, y))
+
     for pos in snake:
         screen.blit(snake_skin, pos)
 
