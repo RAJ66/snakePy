fc3134393a15043c8488f3105e076b2face64f28
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-11-09 17:18:57 +0000

    add score

--- snake.py
+++ snake.py
@@ -1,4 +1,3 @@
-# https://github.com/filhoweuler/Pygame-Snake/blob/master/Solu%C3%A7%C3%A3o%20-%20Snake.pdf
 import pygame
 import random
 from pygame.locals import *
@@ -33,6 +32,9 @@ snake = [(200, 200), (210, 200), (220, 200)]
 my_directio = LEFT
 clock = pygame.time.Clock()
 
+font = pygame.font.Font('freesansbold.ttf', 18)
+score = 0
+
 while True:
     clock.tick(20)
     for event in pygame.event.get():
@@ -51,6 +53,7 @@ while True:
     if collision(snake[0], apple_pos):
         apple_pos = on_grid_rando()
         snake.append((0, 0))
+        score = score+1
 
     if snake[0][0] == 600 or snake[0][1] == 600 or snake[0][0] < 0 or snake[0][1] < 0:
         pygame.quit()
@@ -81,6 +84,11 @@ while True:
         pygame.draw.line(screen, (40, 40, 40), (x, 0), (x, 600))
     for y in range(0, 600, 10):  # Draw vertical lines
         pygame.draw.line(screen, (40, 40, 40), (0, y), (600, y))
+
+    score_font = font.render('Score: %s' % (score), True, (255, 255, 255))
+    score_rect = score_font.get_rect()
+    score_rect.topleft = (600 - 120, 10)
+    screen.blit(score_font, score_rect)
 
     for pos in snake:
         screen.blit(snake_skin, pos)
